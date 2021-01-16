a380a3308aa3dd2e0a0010916497e7e9bcc82ea5
Chris <chrisganeymedia@gmail.com>
2021-01-16 12:56:00 -0600

    adding package.json

--- options-page.php
+++ options-page.php
@@ -1,5 +1,5 @@
 <?php
-   /*
+  /* 
    Plugin Name: optionspage
    Plugin URI: https://github.com
    description: >-
@@ -9,24 +9,26 @@
    Author URI: https://github.com
    License: GPL2
    */
+?>
+<?php
 // add options page
 
 add_action('admin_menu', 'add_gcf_interface');
 function add_gcf_interface() {
-   $my_page = add_options_page('Global Custom Fields', 'Global Custom Fields', 'manage_options', 'functions', 'editglobalcustomfields');
+   $my_page = add_options_page('Global Custom Fields', 'Global Custom Fields', 'manage_options', 'functions', 'options_page_saved_opts');
 
    add_action( 'admin_init', 'register_option_settings' );
 }
 
-function register_option_settings() {
+/*function register_option_settings() {
 	//register our settings
 	register_setting( 'option-settings-group', 'header_text_1' );
 	register_setting( 'option-settings-group', 'header_image_1' );
 	register_setting( 'option-settings-group', 'sidebar_text_1' );
 	register_setting( 'option-settings-group', 'sidebar_image_1' );
-}
+}*/
 
-function editglobalcustomfields() {
+/*function editglobalcustomfields() {
     ?>
     <div class="wrap options-page-global-fields">
       <h2>Global Custom Fields</h2>
@@ -71,48 +73,67 @@ function editglobalcustomfields() {
         </div>
       </form>
 
-      <!-- Place to add new option -->
-
-        
-        
-
-          <div class="add_new_option_wrap">
-            <button id="add_new">ADD NEW</button>
-            <div class="new_option_specs">
-              <form id="add_option" method="post" action="options.php" name="option_addition">
-                <?php wp_nonce_field('update-options') ?>
-                <div class="input_wrap">
-                  <p><strong>Field Name</strong></p>
-                  <input type="text" name="field_name" size="45" form="add_option" value="" placeholder="Field Name" />
-                </div>
-                <div class="input_wrap">
-                  <p><strong>Field Type</strong></p>
-                  <div class="radio_wrap">
-                    <div class="input_wrap radio">
-                      <p>Type Text</p>
-                      <input type="radio" id="type_text" name="field_type" form="add_option" value="text"/>
-                    </div>
-                    <div class="input_wrap radio">
-                      <p>Type Image</p>
-                      <input type="radio" id="type_image" name="field_type" form="add_option" value="image"/>
-                    </div>
-                  </div>
-                </div>
-                <div class="input_wrap submit_wrap">
-                    <input type="submit" name="Submit" id="add_new_option" form="add_option" value="Add Option" />
-                    <input type="hidden" name="action" form="add_option" value="update" />
-                    <input type="hidden" name="page_options" form="add_option" value="field_name, field_type" />
-                </div>
-              </form>
-            </div>
-          </div>
-        
-        <!-- End Place to add new option -->
+      
     </div>
     <?php
-}
+}*/
 
-  function options_page_enqueue() {
+ /* function options_page_enqueue() {
+      wp_enqueue_media();
+      wp_register_script('options-page', plugin_dir_url( __FILE__ ) . 'js/options-page.js', array('jquery'), '1.0.0', true);
+      wp_enqueue_script('options-page');
+      wp_register_style('options-page', plugin_dir_url( __FILE__ ) . 'css/options-page.css', array(), '2.3.4', 'all');
+      wp_enqueue_style('options-page');
+  }
+  add_action('admin_enqueue_scripts', 'options_page_enqueue');*/
+?>
+
+<!-- Place to add new option -->
+
+        
+
+<?php
+function options_page_saved_opts(){ ?>
+	<div class="options-page-global-fields">
+      <h2>Global Custom Fields</h2>
+		  <div class="add_new_option_wrap">
+		  	<div class="add_new_btn_wrap clearfix">
+				<div id="add_new">ADD NEW</div>
+			</div> 
+		    <div class="new_option_specs">
+		      <form id="add_option" method="post" action="options.php" name="option_addition">
+		        <?php wp_nonce_field('update-options') ?>
+		        <?php settings_fields( 'options_page_saved_opts' ); ?>
+				<?php do_settings_sections( 'options_page_saved_opts' ); ?>
+		        <div class="input_wrap">
+		          <p><strong>Field Name</strong></p>
+		          <input type="text" name="field_name" size="45" form="add_option" value="" placeholder="Field Name" />
+		        </div>
+		        <div class="input_wrap">
+		          <p><strong>Field Type</strong></p>
+		          <div class="radio_wrap">
+		            <div class="input_wrap radio">
+		              <p>Type Text</p>
+		              <input type="radio" id="type_text" name="field_type" form="add_option" value="text"/>
+		            </div>
+		            <div class="input_wrap radio">
+		              <p>Type Image</p>
+		              <input type="radio" id="type_image" name="field_type" form="add_option" value="image"/>
+		            </div>
+		          </div>
+		        </div>
+		        <div class="input_wrap submit_wrap">
+		            <input type="submit" name="Submit" id="add_new_option" form="add_option" value="Add Option" />
+		            <!-- <input type="hidden" name="action" form="add_option" value="update" />
+		            <input type="hidden" name="page_options" form="add_option" value="field_name, field_type" /> -->
+		        </div>
+		      </form>
+		    </div>
+		  </div>
+		</div>
+  <?php } 
+
+ function options_page_enqueue() {
       wp_enqueue_media();
       wp_register_script('options-page', plugin_dir_url( __FILE__ ) . 'js/options-page.js', array('jquery'), '1.0.0', true);
       wp_enqueue_script('options-page');
@@ -120,4 +141,9 @@ function editglobalcustomfields() {
       wp_enqueue_style('options-page');
   }
   add_action('admin_enqueue_scripts', 'options_page_enqueue');
-?>
+
+
+
+
+  ?>      
+        <!-- End Place to add new option -->
