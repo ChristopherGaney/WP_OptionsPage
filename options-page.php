90fac2a42e6b30b0fc784bb5c10db9141b6723f8
Chris <chrisganeymedia@gmail.com>
2021-01-17 20:12:21 -0600

    getting ready to add content fields

--- options-page.php
+++ options-page.php
@@ -12,8 +12,6 @@
 ?>
 
 <?php
-// add options page
-
 
 add_action('admin_menu', 'add_gcf_interface');
 function add_gcf_interface() {
@@ -23,66 +21,62 @@ function add_gcf_interface() {
 }
 
 function register_option_settings() {
-  //register our settings
   $opts = get_option('field_array');
   foreach ($opts as $name) {
     register_setting( 'option-settings-group', $name);
   }
-  //register_setting( 'option-settings-group', get_option('field_array')[1]);
-  //register_setting( 'option-settings-group', 'sidebar_text_1' );
-  //register_setting( 'option-settings-group', 'sidebar_image_1' );
 }
 
+function options_page_saved_opts(){ 
 
-?>
-
-
-<?php
-
-
-function options_page_saved_opts(){ ?>
-<?php //$option_sets = [];
-//echo gettype($option_sets); ?>
-<?php 
-
-global $new_whitelist_options;
-$option_names = $new_whitelist_options[ 'option-settings-group' ];
-echo $option_names[0];
-add_option( 'field_array', $option_names );
+  global $new_whitelist_options;
+  $option_names = $new_whitelist_options[ 'option-settings-group' ];
+  add_option( 'field_array', $option_names );
+  echo $option_names[0];
+  echo 'break <br>';
+  $ct = count($option_names);
+  echo $ct;
+  $unique_num = get_option('unique_num');
+  echo $unique_num;
+  if(empty($unique_num)) {
+    add_option('unique_num', $ct + 1);
+    $unique_num = $ct + 1;
+  }
 ?>
 
   <div class="options-page-global-fields">
       <h2>Global Custom Fields</h2>
       <div class="page_option_wrap">
         <div class="page_option_specs">
-          <form id="add_option" method="post" action="options.php" name="option_addition">
+          <form method="post" action="options.php" name="option_addition">
             <?php wp_nonce_field('update-options'); 
+              $count = 1; ?>
+              <div id="add_option" class="fields_holder">
+             <?php foreach ($option_names as $option_name) : ?>
+                <?php $options = get_option($option_name); ?>
 
-            foreach ($option_names as $option_name) : ?>
-              <?php $options = get_option($option_name); ?>
-
-                <div class="field_wrap">
-                  <div class="input_wrap">
-                    <p><strong>Field Name</strong></p>
-                    <input type="text" name="<?php echo $option_name; ?>[field_name]" size="45" form="add_option" value="<?php echo $options['field_name']; ?>" placeholder="Field Name" />
-                  </div>
-                  <div class="input_wrap">
-                    <p><strong>Field Type</strong></p>
-                    <div class="radio_wrap">
-                      <?php $radio = $options['field_type']; ?>
-                      <div class="input_wrap radio">
-                        <p>Type Text</p>
-                        <input type="radio" id="type_text" name="<?php echo $option_name; ?>[field_type]" form="add_option" value="text" <?php checked($radio == 'text'); ?>/>
-                      </div>
-                      <div class="input_wrap radio">
-                        <p>Type Image</p>
-                        <input type="radio" id="type_image" name="<?php echo $option_name; ?>[field_type]" form="add_option" value="image" <?php checked($radio == 'image'); ?>/>
+                  <div class="field_wrap">
+                    <div class="input_wrap">
+                      <p><strong>Field Name</strong></p>
+                      <input type="text" name="<?php echo $option_name; ?>[field_name]" size="45" form="add_option" value="<?php echo $options['field_name']; ?>" placeholder="Field Name" />
+                    </div>
+                    <div class="input_wrap">
+                      <p><strong>Field Type</strong></p>
+                      <div class="radio_wrap">
+                        <?php $radio = $options['field_type']; ?>
+                        <div class="input_wrap radio">
+                          <p>Type Text</p>
+                          <input type="radio" id="type_text" name="<?php echo $option_name; ?>[field_type]" form="add_option" value="text" <?php checked($radio == 'text'); ?>/>
+                        </div>
+                        <div class="input_wrap radio">
+                          <p>Type Image</p>
+                          <input type="radio" id="type_image" name="<?php echo $option_name; ?>[field_type]" form="add_option" value="image" <?php checked($radio == 'image'); ?>/>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-           <?php endforeach; ?>
-
+             <?php $count++; endforeach; ?>
+           </div>
 
             <div class="add_new_btn_wrap clearfix">
               <div id="add_new">ADD NEW</div>
@@ -91,21 +85,23 @@ add_option( 'field_array', $option_names );
             <div id="new_option_specs" class="field_wrap new_spec_field">
               <div class="input_wrap">
                 <p><strong>Field Name</strong></p>
-                <input type="text" name="<?php echo $option_name; ?>[field_name]" size="45" form="add_option" value="<?php echo $options['field_name']; ?>" placeholder="Field Name" />
+                <input type="text" name="dat_set_<?php echo $unique_num; ?>[field_name]" size="45" form="add_option" value="" placeholder="Field Name" />
               </div>
               <div class="input_wrap">
                 <p><strong>Field Type</strong></p>
                 <div class="radio_wrap">
-                  <?php $radio = $options['field_type']; ?>
                   <div class="input_wrap radio">
                     <p>Type Text</p>
-                    <input type="radio" id="type_text" name="<?php echo $option_name; ?>[field_type]" form="add_option" value="text" <?php checked($radio == 'text'); ?>/>
+                    <input type="radio" id="type_text" name="dat_set_<?php echo $unique_num; ?>[field_type]" form="add_option" value="text"/>
                   </div>
                   <div class="input_wrap radio">
                     <p>Type Image</p>
-                    <input type="radio" id="type_image" name="<?php echo $option_name; ?>[field_type]" form="add_option" value="image" <?php checked($radio == 'image'); ?>/>
+                    <input type="radio" id="type_image" name="dat_set_<?php echo $unique_num; ?>[field_type]" form="add_option" value="image"/>
                   </div>
                 </div>
+              </div>
+              <div class="create_btn_wrap clearfix">
+                <div id="create">CREATE</div>
               </div>
             </div>
 
--- src/js/options-page.js
+++ src/js/options-page.js
@@ -1,10 +1,31 @@
+var newHTMLfield = `<div class="field_wrap next_spec_field">
+              <div class="input_wrap">
+                <p><strong>Field Name</strong></p>
+                <input type="text" name="dat_set_<?php echo $count_dracula; ?>[field_name]" size="45" form="add_option" value="" placeholder="Field Name" />
+              </div>
+              <div class="input_wrap">
+                <p><strong>Field Type</strong></p>
+                <div class="radio_wrap">
+                  <div class="input_wrap radio">
+                    <p>Type Text</p>
+                    <input type="radio" id="type_text" name="dat_set_<?php echo $count; ?>[field_type]" form="add_option" value="text"/>
+                  </div>
+                  <div class="input_wrap radio">
+                    <p>Type Image</p>
+                    <input type="radio" id="type_image" name="dat_set_<?php echo $count; ?>[field_type]" form="add_option" value="image"/>
+                  </div>
+                </div>
+              </div>`;
+
+
 jQuery(document).ready(function($){
   var mediaUploader;
   $('#add_new').click(function(e) {
-  	$('#new_option_specs').addClass('showing');
+  	$('#new_option_specs').toggleClass('showing');
   });
-  $('#add_new_option').click(function(e) {
-  	// herre we add new option w/ ajax
+  $('#create').click(function(e) {
+  	 $('#new_option_specs').toggleClass('showing');
+     $('#add_option').append(newHTMLfield); 
   });
   $('.upload_image_button').click(function(e) {
     var url,img;
